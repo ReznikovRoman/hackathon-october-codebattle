118265847410783af3ef938beb9498604e7d5a73
Roman Reznikov <romanreznikov2002@yandex.ru>
2023-12-26 01:41:10 -0500

    chore: update DI

--- src/hackathon/containers.py
+++ src/hackathon/containers.py
@@ -1,10 +1,14 @@
 from dependency_injector import containers, providers
 
+from starlite import Starlite
+
 from hackathon.config.settings import get_settings
 from hackathon.domain import advocates, companies
 from hackathon.infrastructure.db import postgres, redis
 
 __all__ = ["Container", "override_providers"]
+
+from hackathon.infrastructure.db.postgres import Repo, Service, get_db
 
 settings = get_settings()
 
@@ -14,6 +18,7 @@ class Container(containers.DeclarativeContainer):
 
     wiring_config = containers.WiringConfiguration(
         modules=[
+            "hackathon.main",
             "hackathon.api.v1.handlers.advocates",
             "hackathon.api.v1.handlers.companies",
             "hackathon.api.v1.handlers.social_accounts",
@@ -23,11 +28,28 @@ class Container(containers.DeclarativeContainer):
 
     config = providers.Configuration()
 
+    app = providers.Object()
+
     # Infrastructure
 
-    db = providers.Singleton(
+    db = providers.Factory(
         postgres.Database,
         config=settings.database,
+    )
+
+    db_session = providers.Resource(
+        get_db,
+        config=settings.database,
+        app=app,
+    )
+
+    repo = providers.Factory(
+        Repo,
+        session=db_session,
+    )
+    service = providers.Factory(
+        Service,
+        repo=repo,
     )
 
     redis_connection = providers.Resource(
@@ -74,6 +96,11 @@ def override_providers(container: Container, /) -> Container:
     """Override providers with stubs."""
     if not container.config.USE_STUBS():
         return container
+    return container
+
+
+def inject_app(container: Container, app: Starlite, /) -> Container:
+    container.app.override(providers.Object(app))
     return container
 
 
